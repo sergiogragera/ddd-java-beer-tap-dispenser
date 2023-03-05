892e7bf81b4cdf1339acb5b3b5e9d80566debcd0
Sergio Gragera <sergiogragera@gmail.com>
2023-03-05 08:56:10 +0000

    test: add tap build unit test

---
+++ src/test/java/com/rviewer/skeletons/domain/models/TapUnitTest.java
@@ -0,0 +1,46 @@
+package com.rviewer.skeletons.domain.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.Test;
+
+public class TapUnitTest {
+    @Test
+    public void withoutDatesShouldReturnClosedTap() {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser);
+      assertNotNull(tap);
+      assertEquals(1, tap.getId());
+      assertEquals(dispenser, tap.getDispenser());
+      assertFalse(tap.isOpened());
+      assertEquals(0, tap.getLitersDispensed());
+    }
+
+    @Test
+    public void withOpenDateShouldReturnOpenedTap() {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS));
+      assertNotNull(tap);
+      assertEquals(1, tap.getId());
+      assertEquals(dispenser, tap.getDispenser());
+      assertTrue(tap.isOpened());
+      assertTrue(tap.getLitersDispensed() > 0);
+    }
+
+    @Test
+    public void withOpenAndCloseDateShouldReturnClosedTap() {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS),  LocalDateTime.now());
+      assertNotNull(tap);
+      assertEquals(1, tap.getId());
+      assertEquals(dispenser, tap.getDispenser());
+      assertFalse(tap.isOpened());
+      assertEquals(5, tap.getLitersDispensed());
+    }
+}
