38b7c923f343ffe2d6f8119ea3ffc4b616471335
Sergio Gragera <sergiogragera@gmail.com>
2023-03-05 08:55:07 +0000

    feat: add dispenser model

---
+++ src/main/java/com/rviewer/skeletons/domain/models/Dispenser.java
@@ -0,0 +1,26 @@
+package com.rviewer.skeletons.domain.models;
+
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+public class Dispenser {
+    private int id;
+    private LocalDate createdAt = LocalDate.now();
+    private float flowVolume;
+
+    public Dispenser(int id, float flowVolume) {
+        this(id, flowVolume, null);
+    }
+
+    public Dispenser(int id, float flowVolume, LocalDate createdAt) {
+        this.id = id;
+        if (flowVolume <= 0) {
+            throw new RuntimeException();
+        }
+        this.flowVolume = flowVolume;
+        if (createdAt != null) {
+            this.createdAt = createdAt;
+        }
+    }
+}
