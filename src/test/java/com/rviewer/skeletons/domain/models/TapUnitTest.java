98e6966ec9742014acca69b4a79f18a471703303
Sergio Gragera <sergiogragera@gmail.com>
2023-03-05 20:24:35 +0000

    test: add tap build, open and close unit test

--- src/test/java/com/rviewer/skeletons/domain/models/TapUnitTest.java
+++ src/test/java/com/rviewer/skeletons/domain/models/TapUnitTest.java
@@ -11,45 +11,65 @@ import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
 
+import com.rviewer.skeletons.domain.exceptions.DispenserAlreadyClosedException;
+import com.rviewer.skeletons.domain.exceptions.DispenserAlreadyOpenedException;
+import com.rviewer.skeletons.domain.exceptions.DispenserNotOpenYetException;
 import com.rviewer.skeletons.domain.exceptions.InvalidArgumentException;
-import com.rviewer.skeletons.domain.exceptions.InvalidDateRangeException;
 
 public class TapUnitTest {
     @Test
-    public void withoutDatesShouldReturnClosedTap() {
+    public void buildTapShouldThrowIllegalArgumentExceptionWheIdArgIsNotPositiveNumber () {
+      assertThrows(
+        IllegalArgumentException.class,
+          () -> {
+            new Tap(0, null);
+          });
+    }
+
+    @Test
+    public void buildTapShouldThrowIllegalArgumentExceptionWhenNullDispenserArg () {
+      assertThrows(
+        NullPointerException.class,
+          () -> {
+            new Tap(1, null);
+          });
+    }
+
+    @Test
+    public void buildTapShouldReturnClosedTapWhenNullDatesArgs() {
       final var dispenser = new Dispenser(1, 0.5f);
       final var tap = new Tap(1, dispenser);
       assertNotNull(tap);
-      assertEquals(1, tap.getId());
+      assertEquals(1, tap.getId().getValue());
       assertEquals(dispenser, tap.getDispenser());
       assertFalse(tap.isOpened());
       assertEquals(0, tap.getLitersDispensed());
     }
 
     @Test
-    public void withOpenDateShouldReturnOpenedTap() {
+    public void buildTapShouldReturnOpenedTapWhenOpenDateArg() {
       final var dispenser = new Dispenser(1, 0.5f);
       final var tap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS));
       assertNotNull(tap);
-      assertEquals(1, tap.getId());
+      assertEquals(1, tap.getId().getValue());
       assertEquals(dispenser, tap.getDispenser());
       assertTrue(tap.isOpened());
       assertTrue(tap.getLitersDispensed() > 0);
     }
 
     @Test
-    public void withOpenAndCloseDateShouldReturnClosedTap() {
+    public void buildTapShouldReturnClosedTapWhenOpenDateAndCloseDateArgs() {
       final var dispenser = new Dispenser(1, 0.5f);
       final var tap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS),  LocalDateTime.now());
       assertNotNull(tap);
-      assertEquals(1, tap.getId());
+      assertEquals(1, tap.getId().getValue());
       assertEquals(dispenser, tap.getDispenser());
       assertFalse(tap.isOpened());
       assertEquals(5, tap.getLitersDispensed());
     }
 
     @Test
-    public void withCloseBeforeOpenDateShouldThrowInvalidArgumentException() {
+    public void buildTapShouldThrowInvalidArgumentExceptionWhenCloseDateArgBeforeOpenDateArg() {
       final var dispenser = new Dispenser(1, 0.5f);
 
       Exception exception =
@@ -59,8 +79,85 @@ public class TapUnitTest {
                 new Tap(1, dispenser, LocalDateTime.now(), LocalDateTime.now().minus(10, ChronoUnit.SECONDS));
             });
 
+            
         var expectedMessage = "close date before open";
         var actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void openTapShouldThrowAlreadyOpenedExceptionWhenOpenedAtIsNotNull () {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var openedTap = new Tap(1, dispenser, LocalDateTime.now());
+
+      assertThrows(
+        DispenserAlreadyOpenedException.class,
+          () -> {
+            openedTap.open(LocalDateTime.now());
+          });
+    }
+
+    @Test
+    public void openTapWithNullDateShouldReturnTapWithSameStatus() {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser);
+      final var openedTap = new Tap(1, dispenser, LocalDateTime.now());
+      final var closedTap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS), LocalDateTime.now());
+      
+      assertFalse(tap.isOpened());
+      assertTrue(openedTap.isOpened());
+      assertFalse(closedTap.isOpened());
+      
+      tap.open(null);
+      openedTap.open(null);
+      closedTap.open(null);
+
+      assertFalse(tap.isOpened());
+      assertTrue(openedTap.isOpened());
+      assertFalse(closedTap.isOpened());
+    }
+
+    @Test
+    public void closeTapShouldThrowDispenserNotOpenYetExceptionWhenOpenedAtIsNull () {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser);
+
+      assertThrows(
+        DispenserNotOpenYetException.class,
+          () -> {
+            tap.close(LocalDateTime.now());
+          });
+    }
+    
+    @Test
+    public void closeTapShouldThrowDispenserAlreadyClosedExceptionWhenClosedAtIsNotNull () {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var closedTap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS), LocalDateTime.now());
+
+      assertThrows(
+        DispenserAlreadyClosedException.class,
+          () -> {
+            closedTap.close(LocalDateTime.now());
+          });
+    }
+
+    @Test
+    public void closeTapWithNullDateShouldReturnTapWithSameStatus() {
+      final var dispenser = new Dispenser(1, 0.5f);
+      final var tap = new Tap(1, dispenser);
+      final var openedTap = new Tap(1, dispenser, LocalDateTime.now());
+      final var closedTap = new Tap(1, dispenser, LocalDateTime.now().minus(10, ChronoUnit.SECONDS), LocalDateTime.now());
+      
+      assertFalse(tap.isOpened());
+      assertTrue(openedTap.isOpened());
+      assertFalse(closedTap.isOpened());
+      
+      tap.close(null);
+      openedTap.close(null);
+      closedTap.close(null);
+
+      assertFalse(tap.isOpened());
+      assertTrue(openedTap.isOpened());
+      assertFalse(closedTap.isOpened());
     }
 }
