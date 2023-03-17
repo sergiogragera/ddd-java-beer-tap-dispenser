c45b6376662febbdff856822ebe74f04f585350e
Sergio Gragera <sergiogragera@gmail.com>
2023-03-17 22:31:42 +0000

    feat: add filter to jpa query

--- src/main/java/com/rviewer/skeletons/infrastructure/persistence/jpa/UsageEntityJpaRepository.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/persistence/jpa/UsageEntityJpaRepository.java
@@ -1,8 +1,11 @@
 package com.rviewer.skeletons.infrastructure.persistence.jpa;
 
 import com.rviewer.skeletons.infrastructure.persistence.entities.UsageEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsageEntityJpaRepository extends JpaRepository<UsageEntity, Integer> {}
+public interface UsageEntityJpaRepository extends JpaRepository<UsageEntity, Integer> {
+  List<UsageEntity> findByDispenser_Id(int id);
+}
