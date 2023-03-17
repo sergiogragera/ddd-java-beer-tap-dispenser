fbff9e9ad762d4ad125caabf04f836166d5ccac2
Sergio Gragera <sergiogragera@gmail.com>
2023-03-17 22:29:25 +0000

    feat: add spending dto

--- src/main/java/com/rviewer/skeletons/domain/dtos/SpendingResponse.java
+++ src/main/java/com/rviewer/skeletons/domain/dtos/SpendingResponse.java
@@ -1,14 +1,13 @@
 package com.rviewer.skeletons.domain.dtos;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class SpendingResponse {
-  private List<UsageResponse> usages = Arrays.asList();
+  private ArrayList<UsageResponse> usages = new ArrayList<>();
 
   public float getAmount() {
     return usages.stream().map(UsageResponse::getFlowVolume).reduce(0f, Float::sum);
