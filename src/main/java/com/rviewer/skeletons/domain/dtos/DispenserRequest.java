7dbf1e33f8f3f1c7d66446c9246de103e9349d25
Sergio Gragera <sergiogragera@gmail.com>
2023-03-17 22:26:29 +0000

    feat: add no args constructor

--- src/main/java/com/rviewer/skeletons/domain/dtos/DispenserRequest.java
+++ src/main/java/com/rviewer/skeletons/domain/dtos/DispenserRequest.java
@@ -3,11 +3,13 @@ package com.rviewer.skeletons.domain.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class DispenserRequest {
   @JsonProperty("flow_volume")
   private float flowVolume;
