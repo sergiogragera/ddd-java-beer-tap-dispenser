d9b84fc7a5fad725f0c757ad2561b6ca44215e4c
Sergio Gragera <sergiogragera@gmail.com>
2023-03-26 08:00:30 +0000

    feat: add rest validation

--- src/main/java/com/rviewer/skeletons/infrastructure/controllers/DispenserController.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/controllers/DispenserController.java
@@ -1,13 +1,14 @@
 package com.rviewer.skeletons.infrastructure.controllers;
 
 import com.rviewer.skeletons.application.services.DispenserService;
-import com.rviewer.skeletons.domain.dtos.DispenserRequest;
-import com.rviewer.skeletons.domain.dtos.DispenserResponse;
-import com.rviewer.skeletons.domain.dtos.SpendingResponse;
-import com.rviewer.skeletons.domain.dtos.StatusRequest;
-import com.rviewer.skeletons.infrastructure.persistence.adapters.DateAdapter;
+import com.rviewer.skeletons.domain.dtos.request.DispenserRequest;
+import com.rviewer.skeletons.domain.dtos.request.StatusRequest;
+import com.rviewer.skeletons.domain.dtos.response.DispenserResponse;
+import com.rviewer.skeletons.domain.dtos.response.SpendingResponse;
 
-import javax.websocket.server.PathParam;
+import java.util.UUID;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,17 +25,17 @@ public class DispenserController {
   @Autowired private DispenserService dispenserService;
 
   @PostMapping
-  public DispenserResponse createDispenser(@RequestBody DispenserRequest dispenserRequest) {
+  public DispenserResponse createDispenser(@Valid @RequestBody DispenserRequest dispenserRequest) {
     return dispenserService.create(dispenserRequest);
   }
 
   @PutMapping("/{id}/status")
-  public SpendingResponse updateStatusDispenser(@PathVariable int id, @RequestBody StatusRequest statusRequest) {
+  public SpendingResponse updateStatusDispenser(
+      @PathVariable UUID id, @Valid @RequestBody StatusRequest statusRequest) {
     if (statusRequest.getStatus().toString().equals("open")) {
-        dispenserService.open(id, statusRequest.getUpdatedAt());
-    }
-    else {
-        dispenserService.close(id, statusRequest.getUpdatedAt());
+      dispenserService.open(id, statusRequest.getUpdatedAt());
+    } else {
+      dispenserService.close(id, statusRequest.getUpdatedAt());
     }
     return dispenserService.getUsages(id);
   }
