fa13bf5fbf4c6c089ec1b51897d40e4307f4f410
Sergio Gragera <sergiogragera@gmail.com>
2023-03-30 21:27:07 +0000

    refactor: remove unused imports

--- src/test/java/com/rviewer/skeletons/infrastructure/persistence/DispenserRepositoryIntegrationTest.java
+++ src/test/java/com/rviewer/skeletons/infrastructure/persistence/DispenserRepositoryIntegrationTest.java
@@ -3,9 +3,7 @@ package com.rviewer.skeletons.infrastructure.persistence;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.notNull;
 
 import com.rviewer.skeletons.domain.models.Dispenser;
 import com.rviewer.skeletons.domain.persistence.DispenserRepository;
